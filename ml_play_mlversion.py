215affce2ffba5117061be28501757d06c860288
Bobby-Lai <bobby92307@gmail.com>
2020-04-15 21:52:31 +0800

    abandon ml method, use ball's x position predict method

--- ml_play_mlversion.py
+++ ml_play_mlversion.py
@@ -6,7 +6,7 @@ import numpy as np
 import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( 
     SceneInfo, GameStatus, PlatformAction
-    )
+)
 import os.path as path
 
 
@@ -25,28 +25,37 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
-    
+    filename = path.join(path.dirname(__file__),"save/clf_KMeans_BallAndDirection.pickle")
+    with open(filename, 'rb') as file:
+        clf = pickle.load(file)
 
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
     
-    ball_pre_x = 95         # ball's previous x
-    ball_pre_y = 395        # ball's previous y
-    ball_x = 95
-    ball_y = 395
-    plat_center = 95        # center of the platform = plat_x + 20
-    window_width = 200      # width of the game window
-    slope = 1               # slope of ball's move
+    s = [95,93]
+    def get_direction(ball_x,ball_y,ball_pre_x,ball_pre_y):
+        VectorX = ball_x - ball_pre_x
+        VectorY = ball_y - ball_pre_y
+        if(VectorX>=0 and VectorY>=0):
+            return 0
+        elif(VectorX>0 and VectorY<0):
+            return 1
+        elif(VectorX<0 and VectorY>0):
+            return 2
+        elif(VectorX<0 and VectorY<0):
+            return 3
+        
+        
 
     # 3. Start an endless loop.
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
-
+            
         # 3.2. If the game is over or passed, the game process will reset
         #      the scene and wait for ml process doing resetting job.
         if scene_info.status == GameStatus.GAME_OVER or \
-        scene_info.status == GameStatus.GAME_PASS:
+            scene_info.status == GameStatus.GAME_PASS:
             # Do some stuff if needed
             ball_served = False
 
@@ -58,46 +67,37 @@ def ml_loop():
 
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
-            ball_pre_x = scene_info.ball[0]
-            ball_pre_y = scene_info.ball[1]
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
+            feature = []
+            #feature.append(scene_info.ball[0])      #0 ball_x
+            #feature.append(scene_info.ball[1])      #1 ball_y
             ball_x = scene_info.ball[0]
             ball_y = scene_info.ball[1]
-            plat_center = scene_info.platform[0] + 20
-            '''if ball_y - ball_pre_y < 0:     # ball is moving up
-                if plat_center > 100:
-                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-                    print('LEFT')
-                elif plat_center < 100:
-                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-                    print('RIGHT')
-                else:
-                    comm.send_instruction(scene_info.frame, PlatformAction.NONE)
-                    print('NONE')
-                else:'''
-            if ball_y - ball_pre_y != 0:
-                slope = (ball_y - ball_pre_y) / (ball_x - ball_pre_x)
-            else:
-                slope = 1
-
-            predict_x = ball_x + ((400 - ball_y) / slope)
-            if predict_x > 200:
-                predict_x = 400 - predict_x     # predict_x = 200 - (predict_x - 200)
-            elif predict_x < 0:
-                predict_x = -predict_x
-
-            plat_ball_diff = abs(plat_center - predict_x)
-            ball_pre_x = ball_x
-            ball_pre_y = ball_y
-
-            if plat_ball_diff <= 10:
+            
+            feature.append(scene_info.platform[0])  #2 plat_x
+            feature.append(ball_x - s[0])       #3 vector_x
+            feature.append(ball_y - s[1])       #4 vector_y
+            feature.append(get_direction(ball_x,ball_y,s[0],s[1]))  #5 direction
+            '''print(feature[0])
+            print(feature[1])
+            print(feature[3])
+            print(feature[4])'''
+            feature.append((feature[2] / feature[1]))     #6 slope
+            feature.append(s[0] + ((400 - s[1])/feature[4]))    #7 predict_ball_x
+            
+            s = [ball_x, ball_y]
+            feature = np.array(feature)
+            feature = feature.reshape((-1,6))                
+            y = clf.predict(feature)
+            
+            if y == 0:
                 comm.send_instruction(scene_info.frame, PlatformAction.NONE)
                 print('NONE')
-            elif predict_x < plat_center:
+            elif y == 1:
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
                 print('LEFT')
-            elif predict_x > plat_center:
+            elif y == 2:
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
                 print('RIGHT')
