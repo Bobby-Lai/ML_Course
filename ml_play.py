8b089942c2d7af8f12a9e2a2f101f98da95ce862
賴柏瑜 <bobby92307@gmail.com>
2020-05-14 02:35:33 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -15,8 +15,8 @@ def ml_loop(side: str):
         if player == '1P':
             if scene_info["platform_1P"][0]+20  > (pred-10) and scene_info["platform_1P"][0]+20 < (pred+10):
                 return 0 # NONE
-            elif scene_info["platform_1P"][0]+20 <= (pred-10) : return 2 # goes right
-            else : return 1 # goes left
+            elif scene_info["platform_1P"][0]+20 <= (pred-10) : return 1 # goes right
+            else : return 2 # goes left
         else :
             if scene_info["platform_2P"][0]+20  > (pred-10)and scene_info["platform_2P"][0]+20 < (pred+10):
                 if scene_info["platform_2P"][1]+30-scene_info["ball_speed"][1] > scene_info["ball"][1] : #slice
@@ -43,7 +43,7 @@ def ml_loop(side: str):
                 direction = 3
         X = [-scene_info["ball"][0], -scene_info["ball"][1], direction, -scene_info["blocker"][0],-scene_info["ball_speed"][0],-scene_info["ball_speed"][1]]
         X = np.array(X).reshape((1,-1))
-        pred = model.predict(X)
+        pred = -model.predict(X)
         return move_to(player = '1P',pred = pred)
         '''if scene_info["ball_speed"][1] > 0 : # 球正在向下 # ball goes down
             x = ( scene_info["platform_1P"][1]-scene_info["ball"][1] ) // scene_info["ball_speed"][1] # 幾個frame以後會需要接  # x means how many frames before catch the ball
